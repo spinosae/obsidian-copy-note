c1864670c3e6603c272355611c27e19d0e79a65b
yanghua <yanghua.que@olaparty.sg>
2024-04-14 18:12:20 +0800

    + add command to copy notes

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 import {
 	App,
 	Editor,
+	FuzzySuggestModal,
 	MarkdownView,
 	Modal,
 	Notice,
 	Plugin,
 	PluginSettingTab,
 	Setting,
+	TFile,
+	TFolder,
 } from "obsidian";
 
 // Remember to rename these classes and interfaces!
@@ -79,6 +82,18 @@ export default class MyPlugin extends Plugin {
 			},
 		});
 
+		this.addCommand({
+			id: "copy-note",
+			name: "Copy note",
+			callback: async () => {
+				const folderPicker = new FolderSuggestModal(
+					this.app,
+					this.copyNote
+				);
+				folderPicker.open();
+			},
+		});
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -95,6 +110,56 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {}
+
+	async copyNote(dest: TFolder) {
+		const note = this.app.workspace.getActiveFile();
+		if (!note) {
+			new Notice("No note is active");
+			return;
+		}
+
+		const noteCache = this.app.metadataCache.getFileCache(note);
+		if (!noteCache) {
+			new Notice("No metadata found the active note");
+			return;
+		}
+
+		const { links, embeds } = noteCache;
+		const attachments = [links ?? [], embeds ?? []]
+			.flat()
+			.map((l) => this.app.vault.getAbstractFileByPath(l.link))
+			.filter(
+				(f) => f instanceof TFile && f.extension != "md"
+			) as TFile[];
+
+		const destDir = dest.path;
+		// Copy the note to the destination folder
+		const toCopy: { src: TFile; dest: string }[] = [
+			{
+				src: note,
+				dest: `${destDir}/${note.name}`,
+			},
+		];
+		// Copy all attachments referenced in the note
+		for (const attachment of attachments) {
+			toCopy.push({
+				src: attachment!,
+				dest: `${destDir}/attachments/${attachment!.name}`,
+			});
+		}
+
+		for (const copy of toCopy) {
+			const existing = this.app.vault.getAbstractFileByPath(copy.dest);
+			// skip if it is the same file
+			if (copy.src.path === existing?.path) {
+				continue;
+				// delete if target file already exists
+			} else if (existing) {
+				this.app.vault.delete(existing);
+			}
+			await this.app.vault.copy(copy.src, copy.dest);
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -122,6 +187,37 @@ class SampleModal extends Modal {
 	onClose() {
 		const { contentEl } = this;
 		contentEl.empty();
+	}
+}
+
+type folderModalCallback = (folder: TFolder) => Promise<void>;
+class FolderSuggestModal extends FuzzySuggestModal<TFolder> {
+	callback: folderModalCallback;
+
+	constructor(app: App, callback: folderModalCallback) {
+		super(app);
+		this.callback = callback;
+	}
+
+	getItems(): TFolder[] {
+		const abstractFiles = app.vault.getAllLoadedFiles();
+		const folders: TFolder[] = [];
+
+		abstractFiles.forEach((folder) => {
+			if (folder instanceof TFolder) {
+				folders.push(folder);
+			}
+		});
+
+		return folders;
+	}
+
+	getItemText(item: TFolder): string {
+		return item.name;
+	}
+
+	onChooseItem(item: TFolder, evt: MouseEvent | KeyboardEvent): void {
+		this.callback(item);
 	}
 }
 
